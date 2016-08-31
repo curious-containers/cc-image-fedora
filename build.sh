d872f15c4d2f451fc2bd7addb2162c5d3b2093e6
Christoph Jansen <jansen.christoph@yahoo.de>
2016-08-31 11:36:00 +0200

    added --no-cache=true option for docker build to enforce cloning the latest CC-Container-Worker from github

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ REGISTRY_URL=docker.io/curiouscontainers/cc-image-fedora
 
 docker pull docker.io/fedora:24
 docker pull ${REGISTRY_URL}
-docker build -t ${REGISTRY_URL} .
+docker build --no-cache=true -t ${REGISTRY_URL} .
 docker push ${REGISTRY_URL}
