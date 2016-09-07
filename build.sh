6dd0318b21313431a77d88341b63517eeebb8915
Christoph Jansen <jansen.christoph@yahoo.de>
2016-09-07 12:00:08 +0200

    support git and docker tags

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 REGISTRY_URL=docker.io/curiouscontainers/cc-image-fedora
+TAG=
 
 docker pull docker.io/fedora:24
 docker pull ${REGISTRY_URL}
 docker build --no-cache=true -t ${REGISTRY_URL} .
 docker push ${REGISTRY_URL}
+
+if [[ ! -z ${TAG} ]]; then
+    docker tag ${REGISTRY_URL} ${REGISTRY_URL}:${TAG}
+    docker push ${REGISTRY_URL}:${TAG}
+fi
